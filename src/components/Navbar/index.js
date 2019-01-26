4831c20d6761ce315a23d8d17c947b0c05beaf57
Jorge Baez <jibm_82@yahoo.com.mx>
2019-01-26 00:02:23 -0600

    Improved styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,7 +12,11 @@ function Navbar(props) {
           {props.score === 0 ? "Click an image to begin!" : ""}
         </div>
         <div className="col-md-4">
-          Score: {props.score} | Top Score: {props.topScore}
+          <span>Score: {props.score}</span>
+          <span className="d-none d-md-inline"> | </span>
+          <span className="d-block d-md-inline">
+            Top Score: {props.topScore}
+          </span>
         </div>
       </div>
     </nav>
