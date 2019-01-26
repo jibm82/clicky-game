e8a6b2b43a58d44ee4ccd090197d93f91cac83ba
Jorge Baez <jibm_82@yahoo.com.mx>
2019-01-26 00:09:18 -0600

    Updates

---
+++ precache-manifest.a21bac1ba74067663a6e849fd1b3cfff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "753da1e5da9e912067ca",
+    "url": "/clicky-game/static/js/main.753da1e5.chunk.js"
+  },
+  {
+    "revision": "1cb9c7c0148e21415021",
+    "url": "/clicky-game/static/js/1.1cb9c7c0.chunk.js"
+  },
+  {
+    "revision": "753da1e5da9e912067ca",
+    "url": "/clicky-game/static/css/main.416a2ce6.chunk.css"
+  },
+  {
+    "revision": "1cb9c7c0148e21415021",
+    "url": "/clicky-game/static/css/1.303aecfe.chunk.css"
+  },
+  {
+    "revision": "7508e73cab694f212ff7b44ee42674bf",
+    "url": "/clicky-game/index.html"
+  }
+];
